c391c32be716fdb17be157704370fcde892deac6
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-10-15 18:45:08 +0300

    add: ConfigModule, LoggerModule, cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `/environments/.env.${process.env.NODE_ENV}`,
+            isGlobal: true,
+        }),
+        LoggerModule.forRoot({
+            pinoHttp: {
+                name: 'Http-logs',
+                level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+                transport:
+                    process.env.NODE_ENV === 'development'
+                        ? {
+                              target: 'pino-pretty',
+                              // options: { colorize: true },
+                          }
+                        : undefined,
+            },
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+import { Logger } from 'nestjs-pino';
+import * as path from 'path';
 import { AppModule } from './app.module';
 
+const envFile = process.env.NODE_ENV || 'production' ? '.env.production' : '.env.development';
+
+dotenv.config({ path: path.join(__dirname, `../environments/${envFile}`) });
+
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const app = await NestFactory.create(AppModule, { cors: true, bufferLogs: true });
+    const port = process.env.PORT || 3000;
+
+    app.useLogger(app.get(Logger));
+
+    await app.listen(port, () => {
+        console.log(`Server run on port ${port} (${process.env.NODE_ENV})`);
+    });
 }
 bootstrap();
